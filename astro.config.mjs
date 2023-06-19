fb39d9f213cf17d545cf190482eac35e4adc919c
M1chalS <michalszajner@wp.pl>
2023-06-20 00:18:10 +0200

    First layout of index page with reworked footer and header and formated post block in main site area

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sanity from 'astro-sanity';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(),sanity({
-		projectId: 'my3lxnl3',
-		dataset: 'production',
-		apiVersion: '2021-10-21',
-		useCdn: true,
-	})],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), sanity({
+    projectId: 'my3lxnl3',
+    dataset: 'production',
+    apiVersion: '2021-10-21',
+    useCdn: true
+  }), tailwind()]
 });
