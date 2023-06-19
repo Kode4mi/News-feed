1a7a8a52d83882b29ed91378025343659775a12d
M1chalS <michalszajner@wp.pl>
2023-06-19 16:26:29 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sanity from 'astro-sanity';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(),sanity({
+		projectId: 'my3lxnl3',
+		dataset: 'production',
+		apiVersion: '2021-10-21',
+		useCdn: true,
+	})],
+});
